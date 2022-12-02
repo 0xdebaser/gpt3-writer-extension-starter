545a6a91d51983f61d744399833da6b008bea580
debaser9 <jason@jasonbraswell.com>
2022-12-02 13:26:16 -1000

    _

--- scripts/content.js
+++ scripts/content.js
@@ -1,56 +1,12 @@
-//import findAndReplaceDOMText from "findandreplacedomtext";
+// see https://meet-martin.medium.com/using-javascript-es6-import-export-modules-in-chrome-extensions-f63a3a0d2736
 
-function insert(content) {
-  // Find Calmly editor input section
-  const elements = document.getElementsByClassName("droid");
+"use strict";
 
-  if (elements.length === 0) {
-    return false;
-  }
-
-  const element = elements[0];
-
-  // Grab the first p tag so we can replace it with our injection
-  //   const pToRemove = element.childNodes[0];
-  //   pToRemove.remove();
-  element.innerHTML = "";
-
-  // Split content by \n
-  const splitContent = content.split("\n");
-
-  // Wrap in p tags
-  splitContent.forEach((content) => {
-    const p = document.createElement("p");
-
-    if (content === "") {
-      const br = document.createElement("br");
-      p.appendChild(br);
-    } else {
-      p.textContent = content;
-    }
-
-    // Insert into HTML one at a time
-    element.appendChild(p);
-
-    // On success return true
-    return true;
-  });
-}
-
-function replace() {}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("hello from the content message listener!");
-  if (request.message === "inject") {
-    const { selectionText, replacementText } = request;
-    console.log(selectionText);
-    console.log(replacementText);
-    sendResponse({ status: "success" });
-    // const result = insert(content);
-    // if (!result) {
-    //   sendResponse({ status: "failed" });
-    // } else {
-    //   sendResponse({ status: "success" });
-    // }
-  }
-});
+const script = document.createElement("script");
+script.setAttribute("type", "module");
+script.setAttribute("src", chrome.extension.getURL("main.js"));
+const head =
+  document.head ||
+  document.getElementsByTagName("head")[0] ||
+  document.documentElement;
+head.insertBefore(script, head.lastChild);
